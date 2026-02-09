e2359e1ba58391faf473df52d58a9d2e46b947ee
Aike Gilabert <aikegilabert@gmail.com>
2026-02-09 20:46:15 +0100

    more wip

--- src/include/izumi/instruction_table.h
+++ src/include/izumi/instruction_table.h
@@ -6,10 +6,14 @@
 #include "./instruction.h"
 
 typedef struct {
-    size_t stride;
     size_t length;
     izumi_instruction_t instructions[];
 } izumi_instruction_table_t;
+
+typedef struct {
+    size_t length;
+    izumi_stage_t stas[];
+} izumi_stages_blob_t;
 
 inline izumi_instruction_t * izumi_instruction_table__get(izumi_instruction_table_t * it, size_t index) {
     return (izumi_instruction_t *) &((uint8_t*) it->instructions)[it->stride * index];
